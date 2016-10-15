339c3d4555a3587810d6f47345a0f7816c24b1d5
fluffy <fluffy@beesbuzz.biz>
2016-10-15 03:36:47 -0700

    Hey maybe we should document the tests a little

--- tests/tests.php
+++ tests/tests.php
@@ -29,27 +29,47 @@ function test($base, $url, $expected) {
 	echo '</div>';
 }
 
+# A file in the same directory
 test("http://beesbuzz.biz/foo/bar", "test.jpg", "http://beesbuzz.biz/foo/test.jpg");
+# A file in the root directory
 test("http://beesbuzz.biz/foo/bar", "/test.jpg", "http://beesbuzz.biz/test.jpg");
+# A file in the parent directory
 test("http://beesbuzz.biz/foo/bar/baz", "../test.jpg", "http://beesbuzz.biz/foo/test.jpg");
+# A file more directories up than there are directories to escape
 test("http://beesbuzz.biz/foo/bar/baz/quux", "../../../../../test.jpg", "http://beesbuzz.biz/../../test.jpg");
+
+# The current directory itself
 test("http://beesbuzz.biz/foo/", ".", "http://beesbuzz.biz/foo/");
 test("http://beesbuzz.biz/foo/", "./", "http://beesbuzz.biz/foo/");
 test("http://beesbuzz.biz/foo", ".", "http://beesbuzz.biz/");
 test("http://beesbuzz.biz/foo", "./", "http://beesbuzz.biz/");
+
+# Different server, same scheme
 test("http://beesbuzz.biz/foo/bar", "//sockpuppet.us/test.jpg", "http://sockpuppet.us/test.jpg");
 test("https://beesbuzz.biz/foo/bar", "//sockpuppet.us/test.jpg", "https://sockpuppet.us/test.jpg");
+
+# Ensure queries work right
 test("https://beesbuzz.biz/foo/bar.cgi?hello=goodbye", "moo.cgi?yes=no", "https://beesbuzz.biz/foo/moo.cgi?yes=no");
+test("http://beesbuzz.biz/foo/?qwer=poiu", "bar", "http://beesbuzz.biz/foo/bar");
+test("http://beesbuzz.biz/foo/", "bar?qwer=poiu", "http://beesbuzz.biz/foo/bar?qwer=poiu");
+
+# Users and passwords should transfer for relative links
 test("http://fluffy:poopbutt@beesbuzz.biz/foo/bar", ".", "http://fluffy:poopbutt@beesbuzz.biz/foo/");
 test("http://fluffy:poopbutt@beesbuzz.biz/foo/bar", "/test/url", "http://fluffy:poopbutt@beesbuzz.biz/test/url");
 test("http://spambot@beesbuzz.biz/foo/bar", "/test/url", "http://spambot@beesbuzz.biz/test/url");
+
+# But shouldn't transfer to other servers
 test("https://fluffy:poopbutt@beesbuzz.biz/foo/bar", "//sockpuppet.us/test/url", "https://sockpuppet.us/test/url");
+
+# Port specifiers
 test("https://beesbuzz.biz:8000/foo/bar", "//sockpuppet.us/test/url", "https://sockpuppet.us/test/url");
 test("https://beesbuzz.biz:8000/foo/bar", "/test/url", "https://beesbuzz.biz:8000/test/url");
-test("http://beesbuzz.biz/foo/?qwer=poiu", "bar", "http://beesbuzz.biz/foo/bar");
-test("http://beesbuzz.biz/foo/", "bar?qwer=poiu", "http://beesbuzz.biz/foo/bar?qwer=poiu");
+
+# File paths are fiddly
 test("file:///path/to/file", "other-file", "file:///path/to/other-file");
 test("/path/to/file", "other-file", "/path/to/other-file");
+
+# Anchors are too
 test("http://beesbuzz.biz/test/foo#anchor", "bar", "http://beesbuzz.biz/test/bar");
 test("http://beesbuzz.biz/test/foo", "bar#anchor", "http://beesbuzz.biz/test/bar#anchor");
 
