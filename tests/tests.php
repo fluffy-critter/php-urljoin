fluffy-critter/php-urljoin

commit e6070f5b5a5e3d82a6abf367261b61dfd71ed351

Check availability of 'path' before accessing it
The path for either $base or $rel might be empty. For example:

$base = "https://beesbuzz.biz"

PHP generates an error when attempting to access $pbase['path']
because it isn't set in this case (same applies to $prel)

A minor adjustment is to access the first character of the path using
'substr($prel['path'], 0, 1)' instead of '$prel['path'][0]' because
'$prel['path'][0]' returns an error if 'path' is null, where substr()
returns false!

src/urljoin.php
@@ -33,7 +33,7 @@ function urljoin($base, $rel) {
 	}
 
 	$merged = array_merge($pbase, $prel);
-	if ($prel['path'][0] != '/') {
+	if (array_key_exists('path', $prel) && array_key_exists('path', $pbase) && substr($prel['path'], 0, 1) != '/') {
 		// Relative path
 		$dir = preg_replace('@/[^/]*$@', '', $pbase['path']);
 		$merged['path'] = $dir . '/' . $prel['path'];

commit 74625dd9bc6a4371134324716e989e90bf6e8ada

Fix handling of relative paths
e6070f5 introduced a bug that prevented relative path from working
correctly. This commit fixes the bug and adds tests for relative
paths:

  'path/to/file.ext'
'./path/to/file.ext'

src/urljoin.php
@@ -37,10 +37,24 @@ function urljoin($base, $rel) {
 	}
 
 	$merged = array_merge($pbase, $prel);
-	if (array_key_exists('path', $prel) && array_key_exists('path', $pbase) && substr($prel['path'], 0, 1) != '/') {
-		// Relative path
-		$dir = preg_replace('@/[^/]*$@', '', $pbase['path']);
-		$merged['path'] = $dir . '/' . $prel['path'];
+	
+	// Handle relative paths: 
+	//   'path/to/file.ext'
+	// './path/to/file.ext'
+	if (array_key_exists('path', $prel) && substr($prel['path'], 0, 1) != '/') {
+
+		// Normalize: './path/to/file.ext' => 'path/to/file.ext'
+		if (substr($prel['path'], 0, 2) === './') {
+			$prel['path'] = substr($prel['path'], 2);
+		}
+
+		if (array_key_exists('path', $pbase)) {
+			$dir = preg_replace('@/[^/]*$@', '', $pbase['path']);
+			$merged['path'] = $dir . '/' . $prel['path'];
+		} else {
+			$merged['path'] = '/' . $prel['path'];
+		}
+
 	}
 
 	if(array_key_exists('path', $merged)) {

tests/tests.php
@@ -47,6 +47,11 @@ function test($base, $url, $expected) {
 
 # A file in the same directory
 test("http://beesbuzz.biz/foo/bar", "test.jpg", "http://beesbuzz.biz/foo/test.jpg");
+# A file in a subdirectory
+test("http://beesbuzz.biz/foo/bar", "images/test.jpg", "http://beesbuzz.biz/foo/images/test.jpg");
+test("http://beesbuzz.biz/foo/bar", "./images/test.jpg", "http://beesbuzz.biz/foo/images/test.jpg");
+test("http://beesbuzz.biz/foo/bar/", "images/test.jpg", "http://beesbuzz.biz/foo/bar/images/test.jpg");
+test("http://beesbuzz.biz/foo/bar/", "./images/test.jpg", "http://beesbuzz.biz/foo/bar/images/test.jpg");
 # A file in the root directory
 test("http://beesbuzz.biz/foo/bar", "/test.jpg", "http://beesbuzz.biz/test.jpg");
 # A file in the parent directory